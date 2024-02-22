2f16892a8ac05102419c7ea3e0ace991e6484b60
莫罗-肖理旺 <xiaoliwang@holomatic.com>
2024-02-22 17:31:56 +0800

    更新

--- UnityProject/Assets/Shaw/NewIdea/Character.cs
+++ UnityProject/Assets/Shaw/NewIdea/Character.cs
@@ -7,19 +7,34 @@ public class Character : MonoBehaviour
 {
     public Material _Mat_Character;
 
+    public BindableProperty<int> _Hp;
+    public int atk;
+    private static readonly int MainColor = Shader.PropertyToID("_MainColor");
+
     private void Awake()
     {
         _Mat_Character = GetComponent<MeshRenderer>().material;
+
+
+        _Hp.Register((i) =>
+        {
+            if (i < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        });
     }
 
     public void Select()
     {
-        _Mat_Character.SetColor("_MainColor", Color.cyan);
+        _Mat_Character.SetColor(MainColor, Color.cyan);
     }
     public void NoSelect()
     {
-        _Mat_Character.SetColor("_MainColor", Color.white);
+        _Mat_Character.SetColor(MainColor, Color.white);
     }
+    
+    
 
     // private void OnMouseDown()
     // {
---
+++ UnityProject/Assets/Shaw/NewIdea/CharacterData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableObjects/Character", order = 1)]
+public class CharacterData : ScriptableObject
+{
+}
