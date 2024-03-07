84677d72fc67d70fbe1a7fe92ca6bb496c9481bb
莫罗-肖理旺 <xiaoliwang@holomatic.com>
2024-03-07 11:34:34 +0800

    Feat：调整摇杆UI

--- UnityProject/Assets/GameScripts/HotFix/GameLogic/UI/Sokoban/UI_Launching.cs
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/UI/Sokoban/UI_Launching.cs
@@ -8,32 +8,28 @@ namespace GameLogic
     class UI_Launching : UIWindow
     {
         #region 脚本工具生成的代码
-
         private Button _Btn_Restart;
         private Button _Btn_Menu;
-        private Button _Btn_Right;
         private Button _Btn_Up;
         private Button _Btn_Down;
         private Button _Btn_Left;
-
+        private Button _Btn_Right;
         public override void ScriptGenerator()
         {
             _Btn_Restart = FindChildComponent<Button>("_Btn_Restart");
-            _Btn_Menu    = FindChildComponent<Button>("_Btn_Menu");
-            _Btn_Right   = FindChildComponent<Button>("_Btn_Right");
-            _Btn_Up      = FindChildComponent<Button>("_Btn_Up");
-            _Btn_Down    = FindChildComponent<Button>("_Btn_Down");
-            _Btn_Left    = FindChildComponent<Button>("_Btn_Left");
+            _Btn_Menu = FindChildComponent<Button>("_Btn_Menu");
+            _Btn_Up = FindChildComponent<Button>("BtnBg/_Btn_Up");
+            _Btn_Down = FindChildComponent<Button>("BtnBg/_Btn_Down");
+            _Btn_Left = FindChildComponent<Button>("BtnBg/_Btn_Left");
+            _Btn_Right = FindChildComponent<Button>("BtnBg/_Btn_Right");
             _Btn_Restart.onClick.AddListener(OnClick_Btn_RestartBtn);
             _Btn_Menu.onClick.AddListener(OnClick_Btn_MenuBtn);
-            _Btn_Right.onClick.AddListener(OnClick_Btn_RightBtn);
             _Btn_Up.onClick.AddListener(OnClick_Btn_UpBtn);
             _Btn_Down.onClick.AddListener(OnClick_Btn_DownBtn);
             _Btn_Left.onClick.AddListener(OnClick_Btn_LeftBtn);
+            _Btn_Right.onClick.AddListener(OnClick_Btn_RightBtn);
         }
-
         #endregion
-
         #region 事件
 
         private void OnClick_Btn_RestartBtn()
