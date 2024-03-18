4555365b607524b0271639b06efedac758c2f21e
Shaw <shawsquirrel@gmail.com>
2024-03-18 19:13:42 +0800

    Feat：精简GameRoot

--- UnityProject/Assets/GameScripts/HotFix/GameLogic/Main/GameRoot.cs
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/Main/GameRoot.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using GameBase;
@@ -13,32 +12,11 @@ namespace GameLogic
     {
         public static GameRoot _Instance => Game._GameRoot;
 
-        protected void Awake()
-        {
-            AddListen();
-        }
-
-        private void OnDestroy()
-        {
-            RemoveListen();
-        }
-
-        /// <summary>
-        /// 移除监听
-        /// </summary>
-        private void RemoveListen()
+        public GameRoot(GameObject obj) : base(obj)
         {
         }
 
-        /// <summary>
-        /// 添加监听
-        /// </summary>
-        private void AddListen()
-        {
-        }
-
-
-        #region VideoUI()
+        #region VideoUI
 
         public void CloseVideoUI()
         {
@@ -73,18 +51,17 @@ namespace GameLogic
 
         #endregion
 
-
         #region Flash
 
         public async void StartFlash(Action action = null)
         {
-            _mat = GameModule.Resource.LoadAsset<Material>("Mat_UIFlash").Instantiate();
+            Material _mat = GameModule.Resource.LoadAsset<Material>("Mat_UIFlash").Instantiate();
             GameModule.UI.ShowUI<UI_Flash>(_mat);
             bool isComplete = false;
             DOTween.To(() => -1f, value => _mat.SetFloat("_Add", value), 1f, 1f).OnComplete(() => isComplete = true).SetEase(Ease.Linear);
-            
+
             await UniTask.WaitUntil(() => isComplete);
-            
+
             action?.Invoke();
 
             isComplete = false;
@@ -96,47 +73,6 @@ namespace GameLogic
             GameModule.UI.CloseWindow<UI_Flash>();
         }
 
-        public IEnumerator OpenFlash(Action action = null)
-        {
-            yield return FlashStart();
-            action?.Invoke();
-            yield return FlashEnd();
-        }
-
-        private Material _mat;
-
-        public IEnumerator FlashStart()
-        {
-            _mat = GameModule.Resource.LoadAsset<Material>("Mat_UIFlash");
-            GameModule.UI.ShowUI<UI_Flash>(_mat);
-            bool isComplete = false;
-
-            DOTween.To(() => -1f, value => _mat.SetFloat("_Add", value), 1f, 1f).OnComplete(() => isComplete = true).SetEase(Ease.Linear);
-            while (!isComplete)
-            {
-                yield return null;
-            }
-        }
-
-        public IEnumerator FlashEnd()
-        {
-            // _mat = GameModule.Resource.LoadAsset<Material>("Mat_UIFlash");
-            bool isComplete = false;
-
-            DOTween.To(() => 1f, value => _mat.SetFloat("_Add", value), -1f, 1f).OnComplete(() => isComplete = true).SetEase(Ease.Linear);
-
-            while (!isComplete)
-            {
-                yield return null;
-            }
-
-            GameModule.UI.CloseWindow<UI_Flash>();
-        }
-
         #endregion
-
-        public GameRoot(GameObject obj) : base(obj)
-        {
-        }
     }
 }
